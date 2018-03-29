35f82e500de5ee643c4e71bfdcb91cf17855ae13
LefterisKarampas <mr.lef_21113@hotmail.com>
2018-03-30 00:04:26 +0300

    First Commit

---
+++ config/config.php
@@ -0,0 +1,41 @@
+<?php
+/* ========================================================
+ * OpeneClass 2.2 configuration file
+ * Automatically created by install on 2018-03-29 23:35
+ * ======================================================== */
+
+$urlServer = "http://eponymous.csec.gr/";
+$urlAppend = "";
+$webDir    = "/var/www/eclass/Eponymous.csec.gr/" ;
+
+$mysqlServer = "127.0.0.1:11255";
+$mysqlUser = "root";
+$mysqlPassword = "MVPwLSG72XWGrMI+57O0F//I";
+$mysqlMainDb = "eclass";
+$phpMyAdminURL = "../admin/mysql/";
+$phpSysInfoURL = "../admin/sysinfo/";
+$emailAdministrator = "webmaster@localhost";
+$administratorName = "Διαχειριστής";
+$administratorSurname = "Πλατφόρμας";
+$siteName = "Open eClass";
+
+$telephone = "+30 2xx xxxx xxx";
+$fax = "";
+$emailhelpdesk = "";
+
+$language = "greek";
+
+$Institution = "Ακαδημαϊκό Διαδίκτυο GUNet ";
+$InstitutionUrl = "http://www.gunet.gr/";
+$postaddress = "";
+
+$have_latex = FALSE;
+$close_user_registration = FALSE;
+
+$persoIsActive = TRUE;
+$durationAccount = "126144000";
+
+define("UTF8", true);
+
+
+$encryptedPasswd = true;
